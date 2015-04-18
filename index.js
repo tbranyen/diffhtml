318164e8822921e8766ec5b0ed4188fd2f61c878
Tim Branyen <tim@tabdeveloper.com>
2015-04-17 23:56:41 -0400

    Remove the html2hyperscript dependency

--- index.js
+++ index.js
@@ -3,7 +3,117 @@
 // document.querySelector('main').diffHTML = '<new markup to diff/>';
 //
 var virtualDom = require('virtual-dom');
-var html2hscript = require('html2hyperscript');
+
+var hidden = document.implementation.createHTMLDocument('http://www.w3.org/1999/xhtml', 'html');
+
+var humanizeNodeType = {
+  1: 'tag',
+  2: 'attribute',
+  3: 'text',
+  4: 'cdata',
+  8: 'comment'
+};
+
+var prev = null;
+var next = null;
+var parent = null;
+
+var map = new WeakMap();
+
+function recurse(node, i, all) {
+  var entry = {};
+  var nodeName = node.nodeName;
+  var nodeValue = node.nodeValue;
+  var childNodes = node.childNodes;
+  var parentNode = node.parentNode;
+
+  map.set(node, entry);
+
+  if (nodeName === 'style') {
+    entry.type = 'style';
+  }
+  else if (nodeName === 'script') {
+    entry.type = 'script';
+  }
+  else {
+    entry.type = humanizeNodeType[node.nodeType];
+  }
+
+  var attrs = Array.prototype.slice.call(node.attributes || {}, 0);
+
+  entry.attribs = {};
+
+  for (var i = 0; i < attrs.length; i++) {
+    entry.attribs[attrs[i].name] = attrs[i].value;
+  }
+
+  entry.name = nodeName;
+  entry.children = recurseAll(childNodes);
+  entry.text = nodeValue || null;
+
+  return prev = entry;
+}
+
+function recurseAll(nodes) {
+  var nodes = [].map.call(nodes, recurse).filter(Boolean);
+
+  if (nodes.length) {
+    nodes[0].prev = null;
+  }
+
+  return nodes;
+}
+
+function parseHTML(markup) {
+  var frag = hidden.createDocumentFragment();
+  frag.appendChild(document.createElement('div'));
+  frag.firstChild.innerHTML = markup;
+
+  // Find the correct element.
+  return recurseAll(frag.firstChild.childNodes);
+}
+
+function parseHTML(markup) {
+  var frag = hidden.createDocumentFragment();
+
+  frag.appendChild(document.createElement('div'));
+  frag.firstChild.innerHTML = markup;
+
+  // Find the correct element.
+  return recurseAll(frag.firstChild.childNodes);
+}
+
+function parseElement(elem) {
+  var hasAttribs = Object.keys(elem.attribs).length;
+
+  if (elem.name === '#text') {
+    return JSON.stringify(elem.text);
+  }
+
+  if (hasAttribs) {
+    var old = elem.attribs.class;
+    elem.attribs.className = old;
+    delete elem.attribs.class;
+  }
+
+  var inner = [
+      '"' + elem.name + '"',
+      hasAttribs ? JSON.stringify(elem.attribs) : null,
+      '[' + elem.children.map(parseElement).join(', ') + ']'
+  ]
+  .filter(Boolean)
+  .join(',');
+
+  return 'h(' + inner + ')';
+}
+
+function toHScript(elements) {
+  return elements.map(parseElement).join(',');
+}
+
+function html2hscript(markup) {
+  return toHScript(parseHTML(markup));
+}
 
 Object.defineProperty(Element.prototype, 'diffHTML', {
   set: function(newHTML) {
