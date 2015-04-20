c4801bd520f1be4ebe196434a696ef5e0951bc96
Tim <tim@tabdeveloper.com>
2015-04-20 14:19:36 -0400

    Adds in configurable true and ignores dist by default

--- index.js
+++ index.js
@@ -38,6 +38,7 @@ function html2hscript(markup) {
 }
 
 Object.defineProperty(Element.prototype, 'diffHTML', {
+  configurable: true,
   set: function(newHTML) {
     var oldHTML = this.innerHTML;
     var newH = html2hscript(newHTML.outerHTML || newHTML);
