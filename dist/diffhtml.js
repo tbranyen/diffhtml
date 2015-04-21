20b954aa484dd69c8229a224c02e8c46b436617d
Tim Branyen <tim@tabdeveloper.com>
2015-04-20 23:56:32 -0400

    Moved index into lib

--- dist/diffhtml.js
+++ dist/diffhtml.js
@@ -1,6 +1,6 @@
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.diffhtml = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 var virtualDom = require('virtual-dom');
-var recurseNodes = require('./lib/recurse-nodes');
+var recurseNodes = require('./recurse-nodes');
 
 var namespace = 'http://www.w3.org/1999/xhtml';
 var hidden = document.implementation.createHTMLDocument(namespace, 'html');
@@ -79,7 +79,7 @@ Object.defineProperty(Element.prototype, 'diffHTML', {
   }
 });
 
-},{"./lib/recurse-nodes":2,"virtual-dom":7}],2:[function(require,module,exports){
+},{"./recurse-nodes":2,"virtual-dom":7}],2:[function(require,module,exports){
 var humanizeNodeType = {
   1: 'tag',
   2: 'attribute',
