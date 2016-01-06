7d3ddb315317b56a751a4f2b828a6ac7ee9f15f0
Tim Branyen <tbranyen@netflix.com>
2016-01-06 11:15:34 -0800

    Fix detached trigger signature

--- dist/diffhtml.js
+++ dist/diffhtml.js
@@ -170,7 +170,7 @@ Object.defineProperty(exports, '__esModule', {
   value: true
 });
 
-var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; desc = parent = getter = undefined; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
+var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; desc = parent = undefined; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
 
@@ -1542,7 +1542,7 @@ function process(element, patches) {
 
                 var makeDetached = transition.makePromises('detached', [patch.old]);
 
-                triggerTransition('detached', promises, function () {
+                triggerTransition('detached', makeDetached, function () {
                   // And then empty out the entire contents.
                   patch.old.innerHTML = '';
 
@@ -1846,7 +1846,7 @@ function buildTrigger(allPromises) {
       if (!promises.length) {
         callback(promises);
       } else {
-        Promise.all(promises).then(callback);
+        Promise.all(promises).then(callback, callback);
       }
     } else {
       callback();
