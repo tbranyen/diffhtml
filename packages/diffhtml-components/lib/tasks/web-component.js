834a7c5933858990d21e38c049ae2a3334f0a8e3
Tim Branyen <tim@tabdeveloper.com>
2017-06-07 14:50:55 -0700

    Ensure WebComponent will pass children in initial props

--- packages/diffhtml-components/lib/tasks/web-component.js
+++ packages/diffhtml-components/lib/tasks/web-component.js
@@ -1,6 +1,7 @@
 import { Internals } from 'diffhtml';
 
 const { NodeCache } = Internals;
+const { assign } = Object;
 
 export default function webComponentTask(transaction) {
   return transaction.onceEnded(() => {
@@ -48,6 +49,24 @@ export default function webComponentTask(transaction) {
     }
   });
 }
+
+webComponentTask.syncTreeHook = (oldTree, newTree) => {
+  // Stateful components have a very limited API, designed to be fully
+  // implemented by a higher-level abstraction. The only method ever called is
+  // `render`. It is up to a higher level abstraction on how to handle the
+  // changes.
+  for (let i = 0; i < newTree.childNodes.length; i++) {
+    const oldChild = oldTree && oldTree.childNodes && oldTree.childNodes[i];
+    const newChild = newTree.childNodes[i];
+
+    // If incoming tree is a web component, flatten down to tree for now.
+    if (newChild && customElements.get(newChild.nodeName)) {
+      Object.defineProperty(newChild.attributes, 'children', {
+        get: () => newChild.childNodes,
+      });
+    }
+  }
+};
 
 webComponentTask.createNodeHook = (vTree) => {
   let Constructor = null;
