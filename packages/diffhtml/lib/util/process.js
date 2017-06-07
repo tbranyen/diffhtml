befa376ee033032f5bde86ecb0865128f2f3587c
Tim Branyen <tim@tabdeveloper.com>
2017-06-07 11:12:22 -0700

    Fix accidentally paving over the process object

--- packages/diffhtml/lib/util/process.js
+++ packages/diffhtml/lib/util/process.js
@@ -1,8 +1,3 @@
-const normalize = typeof process !== 'undefined' ? process : {
+export default typeof process !== 'undefined' ? process : {
   env: { NODE_ENV: 'development' },
 };
-
-export default Object.defineProperty({}, 'env', {
-  enumerable: true,
-  get: () => normalize.env,
-});
