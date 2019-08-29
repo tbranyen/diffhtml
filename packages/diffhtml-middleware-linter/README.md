0624ed4fe88f1d3e7cd21a1758c3a5310df07fc6
Tim Branyen <tim@tabdeveloper.com>
2019-08-29 11:43:15 -0700

    Initial work on HTML linter based off htmlhint

---
+++ packages/diffhtml-middleware-linter/README.md
@@ -0,0 +1,21 @@
+# <±/> diffHTML Linter Middleware
+
+Stable Version: 1.0.0-beta.9
+
+Use to validate your markup for inconsistencies and bad practices using
+[HTMLHint](https://github.com/htmlhint/HTMLHint) rules.
+
+##### Installation
+
+``` sh
+npm install diffhtml-middleware-linter
+```
+
+##### Example
+
+``` javascript
+import { use } from 'diffhtml';
+import linter from 'diffhtml-middleware-linter';
+
+use(linter());
+```
---
+++ packages/diffhtml-middleware-linter/rollup.config.js
@@ -0,0 +1,37 @@
+import babel from 'rollup-plugin-babel';
+import nodeResolve from 'rollup-plugin-node-resolve';
+import replace from 'rollup-plugin-replace';
+import Visualizer from 'rollup-plugin-visualizer';
+
+const entries = {
+  min: 'index.js',
+  umd: 'index.js',
+};
+
+const dests = {
+  min: 'dist/linter.min.js',
+  umd: 'dist/linter.js',
+}
+
+const { NODE_ENV = 'umd' } = process.env;
+
+export const input = entries[NODE_ENV];
+export const context = 'this';
+
+export const globals = { diffhtml: 'diff' };
+export const external = ['diffhtml'];
+
+export const output = [{
+  file: dests[NODE_ENV],
+  format: 'umd',
+  exports: 'default',
+  name: 'linter',
+  sourcemap: false,
+}];
+
+export const plugins = [
+  NODE_ENV === 'min' && replace({ 'process.env.NODE_ENV': JSON.stringify('production') }),
+  babel(),
+  nodeResolve({ jsnext: true }),
+  NODE_ENV === 'umd' && Visualizer({ filename: './dist/build-size.html' }),
+];
