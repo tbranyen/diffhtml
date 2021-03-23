954a7ced616d704ef95cbd58a76badf0f545764d
Tim Branyen <tim@tabdeveloper.com>
2021-03-22 20:44:55 -0700

    Cleanup components files

--- packages/diffhtml-components/lib/index.js
+++ packages/diffhtml-components/lib/index.js
@@ -1,5 +1,4 @@
 import * as Internals from './util/internals';
 
 export { default as Component } from './component';
-//export { default as WebComponent } from './web-component';
 export { Internals };
