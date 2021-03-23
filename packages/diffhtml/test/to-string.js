c14e0a884139fa72fd1fc0e80b50c368b366cb8c
Tim Branyen <tim@tabdeveloper.com>
2021-03-22 19:55:36 -0700

    Remove diffhtml-components from diffhtml unit tests

--- packages/diffhtml/test/to-string.js
+++ packages/diffhtml/test/to-string.js
@@ -1,19 +1,9 @@
 import { strictEqual, throws } from 'assert';
 import { html, toString } from '../lib/index';
-import { Component } from 'diffhtml-components';
 import validateMemory from './util/validate-memory';
 
-Component.unsubscribeMiddleware();
-
-const whitespaceEx = /[ ]{2,}|\n/g;
-
 describe('toString', function() {
-  beforeEach(() => {
-    Component.subscribeMiddleware();
-  });
-
   afterEach(() => {
-    Component.unsubscribeMiddleware();
     validateMemory();
   });
 
@@ -100,90 +90,5 @@ describe('toString', function() {
     const expected = `<div><div></div><p></p></div>`;
 
     strictEqual(actual, expected);
-  });
-
-  it('can render components', () => {
-    class MyComponent extends Component {
-      render() {
-        return html`
-          <p>Hello world</p>
-        `;
-      }
-    }
-
-    const actual = toString(html`
-      <div>
-        <${MyComponent}  />
-      </div>
-    `);
-
-    const expected = '<div><p>Hello world</p></div>';
-
-    strictEqual(actual.replace(whitespaceEx, ''), expected);
-  });
-
-  it('can render components with props', () => {
-    class MyComponent extends Component {
-      render({ message }) {
-        return html`
-          <p>${message}</p>
-        `;
-      }
-    }
-
-    const actual = toString(html`
-      <div>
-        <${MyComponent} message="Hello world" />
-      </div>
-    `);
-
-    const expected = `<div><p>Hello world</p></div>`;
-
-    strictEqual(actual.replace(whitespaceEx, ''), expected);
-  });
-
-  it('can render components with dynamic props', () => {
-    class MyComponent extends Component {
-      render({ display }) {
-        return html`
-          <p>${display.message}</p>
-        `;
-      }
-    }
-
-    const actual = toString(html`
-      <div>
-        <${MyComponent} display=${{ message: 'Hello world' }} />
-      </div>
-    `);
-
-    const expected = `<div><p>Hello world</p></div>`;
-
-    strictEqual(actual.replace(whitespaceEx, ''), expected);
-  });
-
-  it('can render components with nested elements', () => {
-    class MyComponent extends Component {
-      render() {
-        return html`
-          <html>
-            <head>
-              <title>Test</title>
-            </head>
-
-            <body>
-            </body>
-          </html>
-        `;
-      }
-    }
-
-    const actual = toString(html`
-      <${MyComponent} />
-    `);
-
-    const expected = `<html><head><title>Test</title></head><body></body></html>`;
-
-    strictEqual(actual.replace(whitespaceEx, ''), expected);
   });
 });
