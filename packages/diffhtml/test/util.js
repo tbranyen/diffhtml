e0bf87a04e6cbcf03e9d0ff391bcc4cf1daf21b3
Tim Branyen <tim@tabdeveloper.com>
2021-06-04 15:14:44 -0500

    Support <> in attributes, Closes GH-227 (#228)
    * Support <> in attributes
    
    * Add tests

--- packages/diffhtml/test/util.js
+++ packages/diffhtml/test/util.js
@@ -145,11 +145,18 @@ describe('Util', function() {
       strictEqual(vTree.childNodes[1].childNodes[0].nodeName, 'p');
     });
 
-    it('will not support brackets in attribute values', () => {
+    it('will support brackets in attribute values', () => {
+      const vTree = parse(`<a data-text="<>"></a>`).childNodes[0];
+
+      strictEqual(vTree.nodeName, 'a');
+      strictEqual(vTree.attributes['data-text'], '<>');
+    });
+
+    it('will support complex markup in attribute values', () => {
       const vTree = parse(`<a data-text="<li class='test'></li>"></a>`).childNodes[0];
 
       strictEqual(vTree.nodeName, 'a');
-      strictEqual(vTree.attributes['data-text'], '\"<li');
+      strictEqual(vTree.attributes['data-text'], '<li class=\'test\'></li>');
     });
 
     it('will parse text siblings next to elements', () => {
