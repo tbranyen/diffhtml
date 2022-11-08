e5c0f120c6b2699943f40211c3f85922cb80ed72
Tim Branyen <tim@tabdeveloper.com>
2022-11-08 15:03:23 -0800

    Improve performance

--- packages/diffhtml-rust-parser/src/parser.rs
+++ packages/diffhtml-rust-parser/src/parser.rs
@@ -1,16 +1,17 @@
-extern crate wee_alloc;
+//extern crate wee_alloc;
 
-use js_sys::Reflect;
 use tl::{Node, Parser};
+use serde_wasm_bindgen::to_value;
 use wasm_bindgen::prelude::*;
+use wasm_bindgen::{intern};
+use js_sys::{Reflect, JsString};
 use js_sys::Array;
 use js_sys::Object;
 
 // Opt for a smaller allocator to save on resources.
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+//#[global_allocator]
+//static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-#[derive(Clone, Debug)]
 #[wasm_bindgen(getter_with_clone)]
 pub struct VTree {
     pub rawNodeName: String,
@@ -29,23 +30,21 @@ pub fn crawl_node(dom_node: &Node, parser: &Parser) -> VTree {
     let mut node_name = "#text".to_string();
 
     match node_tag {
-        Some(htmlTag) => {
-            // Set the node name
-            node_name = htmlTag.name().as_utf8_str().to_string();
+        Some(html_tag) => {
+            node_name = html_tag.name().as_utf8_str().to_string();
 
             // Add the attributes
-            for attr in htmlTag.attributes().iter() {
+            for attr in html_tag.attributes().iter() {
                 let key = attr.0.chars().as_str();
-                let mut val = match attr.1 {
+                let value = match attr.1 {
                     None => { "" }
-                    Some(ref cowStr) => {
-                        let varChar = cowStr.chars();
-
-                        varChar.as_str()
+                    Some(ref cow_str) => {
+                        cow_str.chars().as_str()
                     }
                 };
 
-                Reflect::set(&attributes, &JsValue::from_str(key), &JsValue::from_str(val));
+                // FIXME Look into performance improvements this is a very expensive line.
+                Reflect::set(&attributes, &JsValue::from_str(key), &JsValue::from_str(value));
             }
         }
         None => {}
@@ -54,7 +53,7 @@ pub fn crawl_node(dom_node: &Node, parser: &Parser) -> VTree {
     // Loop over the children and build them up into the child_nodes array.
     for inner_children in dom_node.children().iter() {
         for inner_node in inner_children.top().iter() {
-            let v_tree = crawl_node(inner_node.get(parser).unwrap(), parser).clone();
+            let v_tree = crawl_node(inner_node.get(parser).unwrap(), parser);
             child_nodes.push(&JsValue::from(v_tree));
         }
     }
@@ -79,6 +78,7 @@ pub extern "C" fn parse(markup: &str) -> VTree {
     let dom = tl::parse(markup, tl::ParserOptions::default()).unwrap();
     let parser = dom.parser();
     let child_nodes = Array::new();
+    let fragment_name = "#document-fragment".to_string();
 
     // Use the DOM and map into the VTree structure diffHTML expects.
     for dom_node in dom.children().iter() {
@@ -88,8 +88,8 @@ pub extern "C" fn parse(markup: &str) -> VTree {
 
     // Root node is always a document fragment.
     VTree {
-        rawNodeName: "#document-fragment".to_string(),
-        nodeName: "#document-fragment".to_string(),
+        rawNodeName: fragment_name.clone(),
+        nodeName: fragment_name.clone(),
         nodeValue: "".to_string(),
         childNodes: child_nodes,
         attributes: Object::new(),
