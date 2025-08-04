eacb7611698bc56bc8d75bc3380f1e97d3b884b1
Tim Branyen <tim@tabdeveloper.com>
2025-08-04 19:44:55 -0400

    wasm

--- packages/babel-plugin-transform-diffhtml/babel.config.js
+++ packages/babel-plugin-transform-diffhtml/babel.config.js
@@ -1,11 +1,11 @@
-import { parse } from 'diffhtml-rust-parser';
+//import { parse } from 'diffhtml-rust-parser';
 
 export default {
   env: {
     normal: {
       plugins: [['module:./dist/es/index.js', { createTree: 'html' }]]
     },
-
+/*
     wasm: {
       plugins: [
         ['module:./dist/es/index.js', {
@@ -13,6 +13,6 @@ export default {
           parse,
         }]
       ]
-    }
+    }*/
   },
 };
